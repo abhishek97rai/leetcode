dfd0cef6eac781d5ae384951b37137bf288afa5b
Abhishek Rai <abhishekrai.apj@gmail.com>
2023-10-06 23:07:50 -0700

    Create 572. Subtree of Another Tree - Recursion.java

---
+++ 572. Subtree of Another Tree - Recursion.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null)
+            return true;
+        if (root == null || subRoot == null)
+            return false;
+        if (isSame(root, subRoot))
+            return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public boolean isSame(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (p.val == q.val) {
+            return isSame(p.left, q.left) && isSame(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
+}
