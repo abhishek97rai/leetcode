09b16f18500a9d542872b4083f5b0e2d44830bdb
Abhishek Rai <abhishekrai.apj@gmail.com>
2023-10-07 23:08:03 -0700

    Create 102. Binary Tree Level Order Traversal - Single Queue.java

---
+++ 102. Binary Tree Level Order Traversal - Single Queue.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            int currSize = queue.size();
+            res.add(new ArrayList<>());
+            for (int i = 0; i < currSize; i++) {
+                TreeNode node = queue.poll();
+                res.get(level).add(node.val);
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+            level++;
+        }
+        return res;
+    }
+}
