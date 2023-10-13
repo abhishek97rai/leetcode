95782c8be6b8a6ad76d7868703773b14ac9d9abc
Abhishek Rai <abhishekrai.apj@gmail.com>
2023-10-12 23:22:44 -0700

    Update 73. Set Matrix Zeroes - O(1) Space.java

--- 73. Set Matrix Zeroes - O(1) Space.java
+++ 73. Set Matrix Zeroes - O(1) Space.java
@@ -1,32 +1,37 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean firstRow = false;
+        boolean isFirstColZero = false;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                if (i == 0 && matrix[0][j] == 0) {
-                    firstRow = true;
-                    matrix[0][j] = 999;
-                } else if (i > 0 && matrix[i][j] == 0) {
-                    matrix[i][0] = 999;
-                    matrix[0][j] = 999;
+                if (matrix[i][j] == 0) {
+                    if (j == 0) {
+                        isFirstColZero = true;
+                    } else {
+                        matrix[0][j] = 0;
+                    }
+                    matrix[i][0] = 0;
                 }
             }
         }
-        
-        //traversing bottom right to top left, since we have set first cells of each row and column as zero flag, and that shouldnt be affected till the end
-        for (int i = matrix.length-1; i >= 0; i--) {
-            if (matrix[i][0] == 999) {
-                if (i > 0) {
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0) {
                     Arrays.fill(matrix[i], 0);
-                    continue;
-                } 
-            }
-            for (int j = matrix[0].length-1; j >= 0; j--) {
-                if (matrix[0][j] == 999)
-                    matrix[i][j] = 0;
+                    break;
+                } else {
+                    if (matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
             }
         }
-        if (firstRow)
+        if (matrix[0][0] == 0) {
             Arrays.fill(matrix[0], 0);
+        }
+        if (isFirstColZero) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
