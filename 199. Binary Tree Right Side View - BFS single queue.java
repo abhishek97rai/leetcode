3866c2ceebd41f65ce05caed6e49bd1f8ba7ba13
Abhishek Rai <abhishekrai.apj@gmail.com>
2023-10-24 19:47:25 -0700

    Create 199. Binary Tree Right Side View - BFS single queue.java

---
+++ 199. Binary Tree Right Side View - BFS single queue.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Integer> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            TreeNode node = new TreeNode();
+            for (int i = 0; i < size; i++) {
+                node = queue.poll();
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+            res.add(node.val);
+        }
+        return res;
+    }
+}
