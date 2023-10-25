1cd1a0c0bb0f3e1db33bc4770380104bb53e45f4
Abhishek Rai <abhishekrai.apj@gmail.com>
2023-10-25 00:22:45 -0700

    Create 973. K Closest Points to Origin - O(nlogK) using max heap of points.java

---
+++ 973. K Closest Points to Origin - O(nlogK) using max heap of points.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        int[] distances = new int[points.length];
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(k, (a,b) -> ((b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1])));
+        for (int i = 0; i < points.length; i++) {
+            int[] dist = points[i];
+            maxHeap.add(dist);
+            if (maxHeap.size() > k) {
+                maxHeap.remove();
+            }
+        }
+        int[][] res = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            res[i] = maxHeap.remove();
+        }
+        return res;
+    }
+}
