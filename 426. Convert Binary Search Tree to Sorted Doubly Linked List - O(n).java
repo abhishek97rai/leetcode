24dad06eb574ee5ca524610935af862eb06e899c
Abhishek Rai <abhishekrai.apj@gmail.com>
2024-03-29 00:11:59 -0700

    Create 426. Convert Binary Search Tree to Sorted Doubly Linked List - O(n).java

---
+++ 426. Convert Binary Search Tree to Sorted Doubly Linked List - O(n).java
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+
+class Solution {
+
+    Node first = null;
+    Node last = null;
+
+    public Node treeToDoublyList(Node root) {
+        if (root == null)
+            return null;
+        helper(root);
+        first.left = last;
+        last.right = first;
+        return first;    
+    }
+
+    public void helper(Node node) {
+        if (node == null)
+            return;
+        helper(node.left);
+        if (last != null) {
+            last.right = node;
+            node.left = last;
+        } else {
+            first = node;
+        }
+        last = node;
+        helper(node.right);
+    }
+}
